4101932bb335ba12dec0e9b7f866b9c33c73012c
Maor Ben-Yair <benyairmaor@gmail.com>
2022-06-02 18:06:47 +0300

    xxxxxx

--- References/Sinkhorn_RANSAC_ICP_Refernce.py
+++ References/Sinkhorn_RANSAC_ICP_Refernce.py
@@ -7,9 +7,10 @@ import UtilitiesReference as UR
 
 if __name__ == '__main__':
 
-    directories = ['apartment', 'hauptgebaude', 'wood_autumn', 'gazebo_summer', 'gazebo_winter',
-                   'wood_summer', 'stairs', 'plain']
+    directories = ['apartment', 'hauptgebaude', 'wood_autumn', 'gazebo_summer', 'gazebo_winter', 'wood_summer', 'stairs', 'plain']
     results = []
+    matches = []
+    size_dataset = []
     avg_result_datasets = []
     iter_dataset = 0
     sum = 0
@@ -17,19 +18,21 @@ if __name__ == '__main__':
     for directory in directories:
         sources, targets, overlaps, translation_M = UR.get_data_global(directory)
         results.append([])
+        matches.append(0)
+        size_dataset.append(len(sources))
         sum = 0
-        for i in range(len(sources)):
+        for i in range(len(sources) - 1, len(sources)):
+            i = 0
             print("directory:" + directory + ", iter:" + str(i + 1) + "/" + str(len(sources)))
             # Save path for source & target pcd.
             source_path = 'Datasets/eth/' + directory + '/' + sources[i]
             target_path = 'Datasets/eth/' + directory + '/' + targets[i]
 
             # Init voxel for less num of point clouds.
-            voxel_size = 0.2  # means 20cm for this dataset
+            voxel_size = 1  # means 20cm for this dataset
 
             # Prepare data set by compute FPFH.
-            source, target, source_down, target_down, source_fpfh, target_fpfh = UR.prepare_dataset_sinkhorn_ransac(voxel_size, source_path, target_path, translation_M[i])
-            aaa = np.asarray(source.points)
+            source, target, source_down, target_down, source_down_c, target_down_c,  source_fpfh, target_fpfh, M_result, listSource, listTarget = UR.prepare_dataset(voxel_size, source_path, target_path, translation_M[i])
             # Prepare source weight for sinkhorn with dust bin.
             source_arr = np.asarray(source_fpfh.data).T
             s = (np.ones((source_arr.shape[0]+1))*(2/3))/source_arr.shape[0]
@@ -53,7 +56,7 @@ if __name__ == '__main__':
             sink = np.asarray(ot.sinkhorn(s, t, M, 100, numItermax=1200, stopThr=1e-9, verbose=False, method='sinkhorn'))
 
             # Take number of top corr from sinkhorn result and print result.
-            corr_size = 100
+            corr_size = 150
             corr = np.zeros((corr_size, 2))
             for j in range(corr_size):
                 max = np.unravel_index(np.argmax(sink, axis=None), sink.shape)
@@ -61,26 +64,50 @@ if __name__ == '__main__':
                 sink[max[0], :] = 0
                 sink[:, max[1]] = 0
             
-            aaa = np.asarray(source.points)
             # For sinkhorn correspondence result - run first glabal(RANSAC) and then local(ICP) regestration
             result_ransac = UR.execute_global_registration_with_corr(source_down, target_down, corr)
+            UR.draw_registration_result(source, target, result_ransac.transformation, "ransac result")
             # Execute local registration by ICP , Originals pcd and the global registration transformation result,
             # print the result and the correspondence point set .
-            aaa = np.asarray(source.points)
             result_icp = UR.refine_registration_sinkhorn_ransac(source, target, result_ransac)
-            # If the result is not bigger then the overlap else if result bigger than the overlap BUT STILL MISMATCH
-            aaa = np.asarray(source.points)
+            
+            # Calculate the score by compare the correspondnce before and after the tarsformtion.
+            source_down_c.transform(result_icp.transformation)
+            fitness_ = 0.
+            M_check = np.asarray(ot.dist(np.asarray(source_down_c.points), np.asarray(target_down_c.points)))
+            for idx in range(len(listSource)):
+                    if M_check[listSource[idx], listTarget[idx]] <= 0.1001:
+                        fitness_ += 1
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.2002:
+                        fitness_ += 0.8
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.3003:
+                        fitness_ += 0.6
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.4004:
+                        fitness_ += 0.4
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.4004:
+                        fitness_ += 0.2
+            
+            fitness = fitness_ / np.sum(M_result)
 
+            matrix_score = np.linalg.norm(result_icp.transformation - np.linalg.inv(translation_M))
+            
+            overlap_score = 0
             if(result_icp.fitness > overlaps[i]):
-                fitness = 2 - (result_icp.fitness / overlaps[i])
+                overlap_score = 2 - (result_icp.fitness / overlaps[i])
             else:
-                fitness = (result_icp.fitness / overlaps[i])
-            results[iter_dataset].append([sources[i] + " " + targets[i], fitness])
-            sum += results[iter_dataset][i][1]
+                overlap_score = (result_icp.fitness / overlaps[i])
+            
+            results[iter_dataset].append([sources[i] + " " + targets[i], fitness, matrix_score, overlap_score])
+            
+            if fitness > 0.7:
+                matches[iter_dataset] += 1
+                sum += results[iter_dataset][i][1]
 
-            print(results[iter_dataset][i][0], "fitness =", fitness)
+            print(results[iter_dataset][i][0], "fitness =", fitness, "matrix_score = ", matrix_score, "overlap_score = ", overlap_score)
             print("avarage score until now =",sum / len(results[iter_dataset]))
             UR.draw_registration_result(source, target, result_icp.transformation, "ICP result")
+            source.transform(result_icp.transformation)
+            i = len(sources) - 1
 
         avg_result_datasets.append([directory, sum / len(results[iter_dataset])])
         print("\navg result of dataset", directory, "is",avg_result_datasets[iter_dataset][1], "\n\n")
@@ -88,6 +115,8 @@ if __name__ == '__main__':
         iter_dataset += 1
 
     total_avg = sum_datasets / len(avg_result_datasets)
+    print()
     for i in range(len(avg_result_datasets)):
-        print(avg_result_datasets[i][0],'\'s score: ', avg_result_datasets[i][1])
+        print(avg_result_datasets[i][0], '\'s score: ', avg_result_datasets[i][1], 'with ', matches[i], 'problems solved from ', size_dataset[i])
+    print()
     print("\ntotal avarage = ", total_avg)
--- References/UtilitiesReference.py
+++ References/UtilitiesReference.py
@@ -3,7 +3,8 @@ import open3d as o3d
 import copy
 import pandas as pd
 import ot
-
+import torch
+from dgl.geometry import farthest_point_sampler
 
 ######################################################################
 ##################         General Functions        ##################
@@ -60,7 +61,29 @@ def draw_registration_result(source, target, transformation, title):
 
 
 # For pre prossecing the point cloud - make voxel and compute FPFH.
-def preprocess_point_cloud(pcd, voxel_size):
+
+def preprocess_point_cloud_part_a(pcd, voxel_size):
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+    return pcd_down
+
+def preprocess_point_cloud_part_b(pcd, voxel_size):
+    radius_normal = voxel_size * 5
+    radius_feature = voxel_size * 10
+    pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=250))
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=500))
+    return pcd_fpfh
+
+def preprocess_point_cloud_with_farthest_point(pcd, voxel_size):
+    radius_normal = voxel_size * 5
+    radius_feature = voxel_size * 10
+    pcd_ = torch.tensor(np.asarray(pcd.points))
+    pcd_down = dgl.farthest_point_down_sample(pcd_,500)
+    pcd_down = o3d.
+    pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=250))
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=500))
+    return pcd_down, pcd_fpfh
+
+def preprocess_point_cloud_with_voxel(pcd, voxel_size):
     radius_normal = voxel_size * 5
     radius_feature = voxel_size * 10
     pcd_down = pcd.voxel_down_sample(voxel_size)
@@ -93,26 +116,29 @@ def findCorrZeroOne(source, target, distanceThreshold):
                 M_result[i,j]=0
     return M_result, listSource, listTarget
 
-######################################################################
-##################        RANSAC_ICP_Refernce       ##################
-######################################################################
-
-
 # For loading the point clouds : return -
 # (original source , original target , voxel down source , voxel down target , FPFH source , FPFH target).
 def prepare_dataset(voxel_size, source_path, target_path, trans_init):
     source = copy.deepcopy(o3d.io.read_point_cloud(source_path))
     target = copy.deepcopy(o3d.io.read_point_cloud(target_path))
-    source_down_c = preprocess_point_cloud_voxel(source, voxel_size * 10)
-    target_down_c = preprocess_point_cloud_voxel(target, voxel_size * 10)
+    source_down_c = preprocess_point_cloud_voxel(source, voxel_size)
+    target_down_c = preprocess_point_cloud_voxel(target, voxel_size)
     M_result, listSource, listTarget = findCorrZeroOne(source_down_c, target_down_c, 0.1001)
+    print(sum(sum(M_result)))
+    # source_down = preprocess_point_cloud_a(source, voxel_size)
+    # target_down = preprocess_point_cloud_a(target, voxel_size)
     draw_registration_result(source, target, np.identity(4), "Target Matching")
     source.transform(trans_init)
     source_down_c.transform(trans_init)
     draw_registration_result(source, target, np.identity(4), "Problem")
-    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
-    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    source_down, source_fpfh = preprocess_point_cloud_with_farthest_point(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud_with_farthest_point(target, voxel_size)
+    draw_registration_result(source_down, target_down, np.identity(4))
     return source, target, source_down, target_down, source_down_c, target_down_c, source_fpfh, target_fpfh, M_result, listSource, listTarget
+
+######################################################################
+##################        RANSAC_ICP_Refernce       ##################
+######################################################################
 
 # Run global regestration by RANSAC
 def execute_global_registration(source_down, target_down, source_fpfh, target_fpfh):
@@ -137,39 +163,17 @@ def refine_registration(source, target, result_ransac):
 ##################  Sinkhorn_RANSAC_ICP_Refernce    ##################
 ######################################################################
 
-# For pre prossecing the point cloud - make voxel and compute FPFH.
-def preprocess_point_cloud_sinkhorn_ransac(pcd, voxel_size):
-    pcd_down = pcd.voxel_down_sample(voxel_size)
-    radius_normal = voxel_size * 5
-    pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=250))
-    radius_feature = voxel_size * 10
-    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=500))
-    return pcd_down, pcd_fpfh
-
-
-# For loading the point clouds : return -
-# (original source , original target , voxel down source , voxel down target , FPFH source , FPFH target).
-def prepare_dataset_sinkhorn_ransac(voxel_size, source_path, target_path, trans_init):
-    source = copy.deepcopy(o3d.io.read_point_cloud(source_path))
-    target = copy.deepcopy(o3d.io.read_point_cloud(target_path))
-    draw_registration_result(source, target, np.identity(4), "Target Matching")
-    source.transform(trans_init)
-    draw_registration_result(source, target, np.identity(4), "Problem")
-    source_down, source_fpfh = preprocess_point_cloud_sinkhorn_ransac(source, voxel_size)
-    target_down, target_fpfh = preprocess_point_cloud_sinkhorn_ransac(target, voxel_size)
-    return source, target, source_down, target_down, source_fpfh, target_fpfh
-
-
 # Run global regestration by RANSAC
 def execute_global_registration_with_corr(source_down, target_down, corr):
-    distance_threshold = 0.1001
+    distance_threshold = 0.2002
     estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint(True)
     ransac_n = 3
     checkers = [o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)]
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold),
+                o3d.pipelines.registration.CorrespondenceCheckerBasedOnNormal(3.24*0.05)]
     criteria = o3d.pipelines.registration.RANSACConvergenceCriteria(1000000, 0.9999)
     seed = 1
-    corr_size = 0.1001
+    corr_size = 0.2002
     corr = o3d.utility.Vector2iVector(corr)
     result = o3d.pipelines.registration.registration_ransac_based_on_correspondence(
         source_down, target_down, corr, corr_size, estimation_method, ransac_n, checkers, criteria, seed)
@@ -178,8 +182,7 @@ def execute_global_registration_with_corr(source_down, target_down, corr):
 # Run local regestration by icp with transformation result from global regestretion such as RANSAC.
 def refine_registration_sinkhorn_ransac(source, target, result_ransac):
     distance_threshold = 0.1001
-    result = o3d.pipelines.registration.registration_icp(
-        source, target, distance_threshold, result_ransac.transformation,
+    result = o3d.pipelines.registration.registration_icp(source, target, distance_threshold, result_ransac.transformation,
         o3d.pipelines.registration.TransformationEstimationPointToPoint(True),
         criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=1200))
     return result
