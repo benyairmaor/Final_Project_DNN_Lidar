fbf9566e52bc6ad166ce1d60acde5c64f50bc3b9
Maor Ben-Yair <benyairmaor@gmail.com>
2022-06-06 20:13:36 +0300

    finito la comadia

--- References/RANSAC_ICP_Reference_keypoints_sensitivity.py
+++ References/RANSAC_ICP_Reference_keypoints_sensitivity.py
@@ -15,16 +15,24 @@ if __name__ == '__main__':
                    'gazebo_summer', 'gazebo_winter', 'wood_summer', 'stairs',  'plain']
     idx_s = [3, 3, 7, 5, 2, 9, 8, 6]
     idx_f = [4, 1, -1, 1, 0, 4, -1, 2]
-    gamma_21s = np.array([0.07, 0.12, 0.18, 0.27, 0.4, 0.5])
-    gamma_32s = np.array([0.07, 0.12, 0.18, 0.27, 0.4, 0.5])
+    gamma_21_32s = np.array([[0.07, 0.07], [0.07, 0.12], [0.07, 0.18], [0.07, 0.27], [0.07, 0.4], [0.07, 0.5],
+                             [0.12, 0.07], [0.12, 0.12], [0.12, 0.18], [
+        0.12, 0.27], [0.12, 0.4], [0.12, 0.5],
+        [0.18, 0.07], [0.18, 0.12], [0.18, 0.18], [
+        0.18, 0.27], [0.18, 0.4], [0.18, 0.5],
+        [0.27, 0.07], [0.27, 0.12], [0.27, 0.18], [
+        0.27, 0.27], [0.27, 0.4], [0.27, 0.5],
+        [0.4, 0.07], [0.4, 0.12], [0.4, 0.18], [
+        0.4, 0.27], [0.4, 0.4], [0.4, 0.5],
+        [0.5, 0.07], [0.5, 0.12], [0.5, 0.18], [0.5, 0.27], [0.5, 0.4], [0.5, 0.5]])
     scores_fitness = np.ones(
-        (2, len(directories), len(gamma_21s), len(gamma_32s))) * -1
+        (2, len(directories), len(gamma_21_32s))) * -1
     scores_overlap = np.ones(
-        (2, len(directories), len(gamma_21s), len(gamma_32s))) * -1
+        (2, len(directories), len(gamma_21_32s))) * -1
     scores_matrix_distance_rotation = np.ones(
-        (2, len(directories), len(gamma_21s), len(gamma_32s))) * -1
+        (2, len(directories), len(gamma_21_32s))) * -1
     scores_matrix_distance_translation = np.ones(
-        (2, len(directories), len(gamma_21s), len(gamma_32s))) * -1
+        (2, len(directories), len(gamma_21_32s))) * -1
     size_dataset = []
     iter_dataset = 0
     voxel_size = 0.1
@@ -46,83 +54,82 @@ if __name__ == '__main__':
         score_per_dataset_matrix_dist_translation = 0
 
         for j in range(2):
-            for gamma_21 in range(len(gamma_21s)):
-                for gamma_32 in range(len(gamma_32s)):
-                    if j == 0:
-                        i = idx_s[iter_dataset]
-                    else:
-                        i = idx_f[iter_dataset]
-                    if idx_s == -1 or idx_f == -1:
-                        break
+            for gamma_21_32 in range(len(gamma_21_32s)):
+                if j == 0:
+                    i = idx_s[iter_dataset]
+                else:
+                    i = idx_f[iter_dataset]
+                if idx_s == -1 or idx_f == -1:
+                    break
 
-                    # Save path for source & target pcd.
-                    source_path = 'Datasets/eth/' + \
-                        directory + '/' + sources[i]
-                    target_path = 'Datasets/eth/' + \
-                        directory + '/' + targets[i]
+                # Save path for source & target pcd.
+                source_path = 'Datasets/eth/' + \
+                    directory + '/' + sources[i]
+                target_path = 'Datasets/eth/' + \
+                    directory + '/' + targets[i]
 
-                    # Prepare data set by compute FPFH.
-                    source, target, source_down, target_down, source_down_c, target_down_c, source_fpfh, target_fpfh, M_result, listSource, listTarget = UR.prepare_dataset(
-                        voxel_size, source_path, target_path, translation_M[i], "keypoints", VISUALIZATION, gamma_21=gamma_21, gamma_32=gamma_32)
+                # Prepare data set by compute FPFH.
+                source, target, source_down, target_down, source_down_c, target_down_c, source_fpfh, target_fpfh, M_result, listSource, listTarget = UR.prepare_dataset(
+                    voxel_size, source_path, target_path, translation_M[i], "keypoints", VISUALIZATION, gamma_21=gamma_21_32[0], gamma_32=gamma_21_32[1])
 
-                    # Execute global registration by RANSAC and FPFH , print the result and the correspondence point set .
-                    result_ransac = UR.execute_global_registration(
-                        source_down, target_down, source_fpfh, target_fpfh)
+                # Execute global registration by RANSAC and FPFH , print the result and the correspondence point set .
+                result_ransac = UR.execute_global_registration(
+                    source_down, target_down, source_fpfh, target_fpfh)
 
-                    # Execute local registration by ICP , Originals pcd and the global registration transformation result,
-                    # print the result and the correspondence point set .
-                    result_icp = UR.refine_registration(
-                        source, target, result_ransac)
+                # Execute local registration by ICP , Originals pcd and the global registration transformation result,
+                # print the result and the correspondence point set .
+                result_icp = UR.refine_registration(
+                    source, target, result_ransac)
 
-                    # Calculate the score by 3 diffenerte approaches
-                    # 1. Compare the correspondnce before and after the tarsformtion. (fitness) as far as target point from source the socre in decreases.
-                    source_down_c.transform(result_icp.transformation)
-                    fitness_ = 0.
-                    M_check = np.asarray(
-                        ot.dist(np.asarray(source_down_c.points), np.asarray(target_down_c.points)))
+                # Calculate the score by 3 diffenerte approaches
+                # 1. Compare the correspondnce before and after the tarsformtion. (fitness) as far as target point from source the socre in decreases.
+                source_down_c.transform(result_icp.transformation)
+                fitness_ = 0.
+                M_check = np.asarray(
+                    ot.dist(np.asarray(source_down_c.points), np.asarray(target_down_c.points)))
 
-                    for idx in range(len(listSource)):
-                        if M_check[listSource[idx], listTarget[idx]] <= 0.1001:
-                            fitness_ += 1
-                        elif M_check[listSource[idx], listTarget[idx]] <= 0.2002:
-                            fitness_ += 0.8
-                        elif M_check[listSource[idx], listTarget[idx]] <= 0.3003:
-                            fitness_ += 0.6
-                        elif M_check[listSource[idx], listTarget[idx]] <= 0.4004:
-                            fitness_ += 0.4
-                        elif M_check[listSource[idx], listTarget[idx]] <= 0.5005:
-                            fitness_ += 0.2
+                for idx in range(len(listSource)):
+                    if M_check[listSource[idx], listTarget[idx]] <= 0.1001:
+                        fitness_ += 1
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.2002:
+                        fitness_ += 0.8
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.3003:
+                        fitness_ += 0.6
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.4004:
+                        fitness_ += 0.4
+                    elif M_check[listSource[idx], listTarget[idx]] <= 0.5005:
+                        fitness_ += 0.2
 
-                    fitness = fitness_ / np.sum(M_result)
+                fitness = fitness_ / np.sum(M_result)
 
-                    # 2. Calculate the overlap beteen the PCDs
-                    overlap_score = 0
+                # 2. Calculate the overlap beteen the PCDs
+                overlap_score = 0
 
-                    if(result_icp.fitness > overlaps[i]):
-                        overlap_score = 2 - (result_icp.fitness / overlaps[i])
-                    else:
-                        overlap_score = (result_icp.fitness / overlaps[i])
+                if(result_icp.fitness > overlaps[i]):
+                    overlap_score = 2 - (result_icp.fitness / overlaps[i])
+                else:
+                    overlap_score = (result_icp.fitness / overlaps[i])
 
-                    # 3. Compute the distanse between the resulp ICP translation matrix and the inverse of the problem matrix
-                    rotaition_score = np.linalg.norm(
-                        result_icp.transformation[:3, :3] - np.linalg.inv(translation_M[i])[:3, :3])
-                    translation_score = np.linalg.norm(
-                        result_icp.transformation[:3, 3:] - np.linalg.inv(translation_M[i])[:3, 3:])
+                # 3. Compute the distanse between the resulp ICP translation matrix and the inverse of the problem matrix
+                rotaition_score = np.linalg.norm(
+                    result_icp.transformation[:3, :3] - np.linalg.inv(translation_M[i])[:3, :3])
+                translation_score = np.linalg.norm(
+                    result_icp.transformation[:3, 3:] - np.linalg.inv(translation_M[i])[:3, 3:])
 
-                    if VISUALIZATION:
-                        UR.draw_registration_result(
-                            source, target, result_icp.transformation, "ICP result")
+                if VISUALIZATION:
+                    UR.draw_registration_result(
+                        source, target, result_icp.transformation, "ICP result")
 
-                    scores_fitness[j][iter_dataset][voxel_size] = fitness
-                    scores_overlap[j][iter_dataset][voxel_size] = overlap_score
-                    scores_matrix_distance_rotation[j][iter_dataset][voxel_size] = rotaition_score
-                    scores_matrix_distance_translation[j][iter_dataset][voxel_size] = translation_score
+                scores_fitness[j][iter_dataset][gamma_21_32] = fitness
+                scores_overlap[j][iter_dataset][gamma_21_32] = overlap_score
+                scores_matrix_distance_rotation[j][iter_dataset][gamma_21_32] = rotaition_score
+                scores_matrix_distance_translation[j][iter_dataset][gamma_21_32] = translation_score
         iter_dataset += 1
 
     for y in range(scores_fitness.shape[1]):
         for x in range(scores_fitness.shape[0]):
-            plt.plot(gamma_21s, scores_fitness[x][y], color='green')
-            plt.plot(gamma_32s, scores_fitness[x][y], color='red')
+            plt.plot(gamma_21_32s, scores_fitness[x][y], color='green')
+
             plt.xlabel('gamma_21/gamma_32 size')
             plt.ylabel('scores (fitness)')
 
@@ -136,8 +143,7 @@ if __name__ == '__main__':
 
             plt.show()
 
-            plt.plot(gamma_21s, scores_overlap[x][y], color='green')
-            plt.plot(gamma_32s, scores_overlap[x][y], color='red')
+            plt.plot(gamma_21_32s, scores_overlap[x][y], color='green')
 
             plt.xlabel('gamma_21/gamma_32 size')
             plt.ylabel('scores (overlap)')
@@ -150,9 +156,7 @@ if __name__ == '__main__':
             plt.show()
 
             plt.plot(
-                gamma_21s, scores_matrix_distance_rotation[x][y], color='green')
-            plt.plot(
-                gamma_32s, scores_matrix_distance_rotation[x][y], color='red')
+                gamma_21_32s, scores_matrix_distance_rotation[x][y], color='green')
             plt.xlabel('gamma_21/gamma_32 size')
             plt.ylabel('scores (matrix distance rotation)')
 
@@ -166,9 +170,7 @@ if __name__ == '__main__':
             plt.show()
 
             plt.plot(
-                gamma_21s, scores_matrix_distance_translation[x][y], color='green')
-            plt.plot(
-                gamma_32s, scores_matrix_distance_translation[x][y], color='red')
+                gamma_21_32s, scores_matrix_distance_translation[x][y], color='green')
             plt.xlabel('gamma_21/gamma_32 size')
             plt.ylabel('scores (matrix distance translation)')
 
